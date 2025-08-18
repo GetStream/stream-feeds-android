4745fd9459476f93b8b071483d5766c1c45b9240
PetarVelikov <petar.velikov@getstream.io>
2025-08-18 16:29:36 +0200

    Add Stream Feeds Android as default production app.

--- stream-feeds-android-sample/src/main/java/io/getstream/feeds/android/sample/DemoAppConfig.kt
+++ stream-feeds-android-sample/src/main/java/io/getstream/feeds/android/sample/DemoAppConfig.kt
@@ -11,20 +11,41 @@ data class DemoAppConfig(
             apiKey = "pd67s34fzpgw",
             token = { userId ->
                 when (userId) {
-                    "luke_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.hZ59SWtp_zLKVV9ShkqkTsCGi_jdPHly7XNCf5T_Ev0"
-//                    /* expired token */ "luke_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIiLCJleHAiOjE3NTMyOTUyMDh9.FhnfaytqAOponkFigQl1_BWIbuHhfubNPKKz4jxfA8I"
                     "petar" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicGV0YXIifQ.mZFi4iSblaIoyo9JDdcxIkGkwI-tuApeSBawxpz42rs"
                     "gian" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2lhbiJ9.DEb-U3KExmEmYLkc9nYA1CFMSYJF-eUZrcDWDJhtiIY"
+                    "luke_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.hZ59SWtp_zLKVV9ShkqkTsCGi_jdPHly7XNCf5T_Ev0"
+                    "leia_organa" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGVpYV9vcmdhbmEifQ.8NXs4DZrx_hljsaC8d6xlZ07FUgenKmb6hDNU-KFQ3M"
+                    "han_solo" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGFuX3NvbG8ifQ.lLYA_RUGZlmWULg-En-7tbTAuoVWFSR1-ad_e7s8PqM"
+                    "lando_calrissian" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGFuZG9fY2Fscmlzc2lhbiJ9.QIxUC5nTo3x1C4bkyEv5b8-pHZwIE5BDeRuBw4Z1K14"
+                    "chewbacca" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiY2hld2JhY2NhIn0.4FJLy1za8OWCS8Bf6fW76w_TGfvJ0Q8o60gLk0qtrnc"
+                    "c3po" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzNwbyJ9._hR-bDio4Ai9hWHhYXuKaiXQ8gyzhll7RRHqj7Eb1Ok"
+                    "r2d2" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicjJkMiJ9.NYxcxPTrB6ov0skhA-Kmvrmf9ewWP94BwHCQROuQ4ec"
+                    "anakin_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYW5ha2luX3NreXdhbGtlciJ9.4Rsce_GZeY9g4SHAVgqkjgqAHl70_8iSHCAYeRSuMY8"
+                    "obi_wan_kenobi" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoib2JpX3dhbl9rZW5vYmkifQ.2BTRdIM1xAy8mTWfAT_y61Mjrkjuwv7X-SQRFO0UJ74"
+                    "padme_amidala" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicGFkbWVfYW1pZGFsYSJ9.4czOc0NE73usN7eSUoWzg6-_sw5BhahE_QRMC-minHc"
+                    "qui_gon_jinn" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicXVpX2dvbl9qaW5uIn0.aetVSDimtIqV2tM5AA-7MsClTmFtVKIr4lzOKOFlv6Q"
                     else -> ""
                 }
             }
         )
 
         val Production = DemoAppConfig(
-            apiKey = "fa5xpkvxrdw4",
+            apiKey = "2wyrh3ssdhfs",
             token = { userId ->
                 when (userId) {
-                    "luke_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.zuAJWZfZWPFJYYItt1QQnUl2IlTq6PcBPStrcD_U91A"
+                    "petar" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicGV0YXIifQ.0-fAFuzhkibafsuiHnVytiYz_6bMqDBFksCvRxrVn2A"
+                    "gian" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2lhbiJ9.KB5fzlZgCz9n8bZNQpLseS2rkpPWrJXLujUrQHf_INo"
+                    "luke_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.P6tZUf7OTEYNUUA4yPOlwN3fWqebDGoARSw09ooeyaY"
+                    "leia_organa" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGVpYV9vcmdhbmEifQ.buNtkyPDegZ4G6NjMghYHIt3vH5nnnLpNh9MjgJaSXI"
+                    "han_solo" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGFuX3NvbG8ifQ.KEeSINlSt7zp-UItfNR9yzvHuPVjAGJqfBGzZcilKZE"
+                    "lando_calrissian" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGFuZG9fY2Fscmlzc2lhbiJ9.fBjR-FBGAV4H6fteW53ggJ4_D_QDMh-0yLFzhMSxJC0"
+                    "chewbacca" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiY2hld2JhY2NhIn0.I3cPFebLRDFPTCagqhFPgsijyb0c6hBOPTcZAlYjGq8"
+                    "c3po" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzNwbyJ9.rmm2E1EaC599gne9eqSWJX3rMY958mQhliRwL9DEu8s"
+                    "r2d2" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicjJkMiJ9.D-GZg_PyffOf7D_WPTVP9SOaRklPVgvuGY3iRTqbeh0"
+                    "anakin_skywalker" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYW5ha2luX3NreXdhbGtlciJ9.WOllUZafwfejx1kErZPrPeXTJYnEFTxj3SQBixaTbVA"
+                    "obi_wan_kenobi" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoib2JpX3dhbl9rZW5vYmkifQ.uJW6FnsPhmLo4p9pKb3QzaIfOV-nNcZumNE1LG1dQls"
+                    "padme_amidala" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicGFkbWVfYW1pZGFsYSJ9.RNTUNRdwLn3x1h2VMBqe1JoS6P88mKWqsHYAihFuxBU"
+                    "qui_gon_jinn" -> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicXVpX2dvbl9qaW5uIn0.NpIlGpn97YurPezcoEiqgGcS5qJu5SoavsE1JcY8lM8"
                     else -> ""
                 }
             }
--- stream-feeds-android-sample/src/main/java/io/getstream/feeds/android/sample/login/UserCredentials.kt
+++ stream-feeds-android-sample/src/main/java/io/getstream/feeds/android/sample/login/UserCredentials.kt
@@ -22,23 +22,117 @@ data class UserCredentials(
             ),
             userToken = UserToken(DemoAppConfig.Current.token("petar")),
         )
-        val Luke = UserCredentials(
-            user = User(
-                id = "luke_skywalker",
-                name = "Luke Skywalker",
-                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"
-            ),
-            userToken = UserToken(DemoAppConfig.Current.token("luke_skywalker"))
-        )
         val Gian = UserCredentials(
             user = User(
                 id = "gian",
                 name = "Gian",
-                imageURL = "https://ca.slack-edge.com/T02RM6X6B-U09645WUWQ3-a89200afc9fd-512"
+                imageURL = "https://ca.slack-edge.com/T02RM6X6B-U09645WUWQ3-a89200afc9fd-512",
             ),
             userToken = UserToken(DemoAppConfig.Current.token("gian"))
         )
+        val Luke = UserCredentials(
+            user = User(
+                id = "luke_skywalker",
+                name = "Luke Skywalker",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("luke_skywalker")),
+        )
+        val Leia = UserCredentials(
+            user = User(
+                id = "leia_organa",
+                name = "Leia Organa",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/f/fc/Leia_Organa_TLJ.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("leia_organa")),
+        )
+        val Han = UserCredentials(
+            user = User(
+                id = "han_solo",
+                name = "Han Solo",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/e/e2/TFAHanSolo.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("han_solo"))
+        )
+        val Lando = UserCredentials(
+            user = User(
+                id = "lando_calrissian",
+                name = "Lando Calrissian",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/8/8f/Lando_ROTJ.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("lando_calrissian")),
+        )
+        val Chewbacca = UserCredentials(
+            user = User(
+                id = "chewbacca",
+                name = "Chewbacca",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("chewbacca")),
+        )
+        val C3PO = UserCredentials(
+            user = User(
+                id = "c3po",
+                name = "C-3PO",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/3/3f/C-3PO_TLJ_Card_Trader_Award_Card.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("c3po")),
+        )
+        val R2D2 = UserCredentials(
+            user = User(
+                id = "r2d2",
+                name = "R2-D2",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("r2d2")),
+        )
+        val Anakin = UserCredentials(
+            user = User(
+                id = "anakin_skywalker",
+                name = "Anakin Skywalker",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("anakin_skywalker")),
+        )
+        val ObiWan = UserCredentials(
+            user = User(
+                id = "obi_wan_kenobi",
+                name = "Obi-Wan Kenobi",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("obi_wan_kenobi")),
+        )
+        val Padme = UserCredentials(
+            user = User(
+                id = "padme_amidala",
+                name = "Padmé Amidala",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/b/b2/Padmegreenscrshot.jpg",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("padme_amidala")),
+        )
+        val QuiGon = UserCredentials(
+            user = User(
+                id = "qui_gon_jinn",
+                name = "Qui-Gon Jinn",
+                imageURL = "https://vignette.wikia.nocookie.net/starwars/images/f/f6/Qui-Gon_Jinn_Headshot_TPM.jpg",
+            ),
+            userToken = UserToken(DemoAppConfig.Current.token("qui_gon_jinn")),
+        )
 
-        val BuiltIn = listOf(Petar, Luke, Gian)
+        val BuiltIn = listOf(
+            Petar,
+            Gian,
+            Luke,
+            Leia,
+            Han,
+            Lando,
+            Chewbacca,
+            C3PO,
+            R2D2,
+            Anakin,
+            ObiWan,
+            Padme,
+            QuiGon
+        )
     }
 }
