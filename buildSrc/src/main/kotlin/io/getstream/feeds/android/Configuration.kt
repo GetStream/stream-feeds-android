d9fa3683d02278819dbdf64c9cd7b9f6e36fc39d
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-08-28 11:38:44 +0200

    Correct version pre-release

--- buildSrc/src/main/kotlin/io/getstream/feeds/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/feeds/android/Configuration.kt
@@ -21,8 +21,8 @@ object Configuration {
     const val sampleTargetSdk = 36
     const val minSdk = 21
     const val majorVersion = 0
-    const val minorVersion = 1
-    const val patchVersion = 0
+    const val minorVersion = 0
+    const val patchVersion = 1
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
