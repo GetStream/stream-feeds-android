1737d7ea5eb0cb121148104e75f8db9a81a0a751
Gian <47775302+gpunto@users.noreply.github.com>
2025-09-19 14:51:38 +0200

    Migrate FeedList to consuming state update events

--- stream-feeds-android-client/src/main/kotlin/io/getstream/feeds/android/client/internal/state/event/StateUpdateEvent.kt
+++ stream-feeds-android-client/src/main/kotlin/io/getstream/feeds/android/client/internal/state/event/StateUpdateEvent.kt
@@ -18,6 +18,7 @@ package io.getstream.feeds.android.client.internal.state.event
 import io.getstream.feeds.android.client.api.model.BookmarkData
 import io.getstream.feeds.android.client.api.model.BookmarkFolderData
 import io.getstream.feeds.android.client.api.model.CommentData
+import io.getstream.feeds.android.client.api.model.FeedData
 import io.getstream.feeds.android.client.api.model.FeedsReactionData
 import io.getstream.feeds.android.client.api.model.toModel
 import io.getstream.feeds.android.network.models.BookmarkDeletedEvent
@@ -29,6 +30,8 @@ import io.getstream.feeds.android.network.models.CommentDeletedEvent
 import io.getstream.feeds.android.network.models.CommentReactionAddedEvent
 import io.getstream.feeds.android.network.models.CommentReactionDeletedEvent
 import io.getstream.feeds.android.network.models.CommentUpdatedEvent
+import io.getstream.feeds.android.network.models.FeedDeletedEvent
+import io.getstream.feeds.android.network.models.FeedUpdatedEvent
 import io.getstream.feeds.android.network.models.WSEvent
 
 /**
@@ -56,6 +59,10 @@ internal sealed interface StateUpdateEvent {
 
     data class CommentReactionDeleted(val comment: CommentData, val reaction: FeedsReactionData) :
         StateUpdateEvent
+
+    data class FeedUpdated(val feed: FeedData) : StateUpdateEvent
+
+    data class FeedDeleted(val fid: String) : StateUpdateEvent
 }
 
 internal fun WSEvent.toModel(): StateUpdateEvent? =
@@ -80,6 +87,10 @@ internal fun WSEvent.toModel(): StateUpdateEvent? =
 
         is CommentReactionDeletedEvent ->
             StateUpdateEvent.CommentReactionDeleted(comment.toModel(), reaction.toModel())
+
+        is FeedUpdatedEvent -> StateUpdateEvent.FeedUpdated(feed.toModel())
+
+        is FeedDeletedEvent -> StateUpdateEvent.FeedDeleted(fid)
 
         else -> null
     }
