b118adeb4d6cf41378614821c6d7c0b5d5a9cfa9
Kanake <10kanake@gmail.com>
2025-10-14 19:56:56 +0300

    fix lint

--- stream-feeds-android-sample/src/main/java/io/getstream/feeds/android/sample/feed/CreatePollBottomSheet.kt
+++ stream-feeds-android-sample/src/main/java/io/getstream/feeds/android/sample/feed/CreatePollBottomSheet.kt
@@ -45,7 +45,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
